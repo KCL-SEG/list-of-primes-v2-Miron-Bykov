11dd84ed9c03a46106b4172eb7c00d35687e9e42
Miron Bykov <miron.bykov@kcl.ac.uk>
2022-12-08 16:56:17 +0000

    complete task

--- primes.py
+++ primes.py
@@ -1,32 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def primes(number_of_primes):
-    list = [ ]
-    total = 0
-    if number_of_primes < 1:
-        raise ValueError("You must enter a positive integer")
-
-    while total < number_of_primes:
-        if len(list) == 0:
-            for i in range(2, number_of_primes + 1):
-                count = 0
-                for j in range(1, i + 1):
-                    if i % j == 0:
-                        count += 1
-                if count < 3:
-                    list.append(i)
-                    total += 1
-        else:
-            #quite inefficient
-            for i in range(list[-1] + 1, list[-1] + 20):
-                if total >= number_of_primes:
-                    break
-                count = 0
-                for j in range(1, i + 1):
-                    if i % j == 0:
-                        count += 1
-                if count < 3:
-                    list.append(i)
-                    total += 1
+def primes(numOfPrimes):
+    if numOfPrimes<1:
+        raise ValueError("ValueError exception thrown")
+    else:
+        list = [2]
+        numToTest=2
+        while len(list)!=numOfPrimes:
+            numToTest+=1
+            if isPrime(numToTest, True):
+                list.append(numToTest)
     return list
+
+def isPrime(number, bool):
+    prime=bool
+    if number%2:
+        for i in range (3,(number+1)//2,2):
+            if number%i==0:
+                prime=False
+                break
+        return prime
